0a63ac2ff79ae60c63181e492ad970580ed012a4
Pramod Dutta <pramoddutta_96@yahoo.com>
2014-11-02 03:16:33 -0800

    Initial commit with layout ready and adapter function

---
+++ app/src/main/java/com/witmergers/batmanwallpapers/MainActivity.java
@@ -0,0 +1,343 @@
+package com.witmergers.batmanwallpapers;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.etsy.android.grid.StaggeredGridView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity implements AbsListView.OnScrollListener, AbsListView.OnItemClickListener {
+
+
+    private static final String TAG = "StaggeredGridActivity";
+    public static final String SAVED_DATA_KEY = "SAVED_DATA";
+
+    private StaggeredGridView mGridView;
+    private boolean mHasRequestedMore;
+    private SampleAdapter mAdapter;
+
+    private ArrayList<String> mData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("Batman Wallpapers");
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext()).build();
+        ImageLoader.getInstance().init(config);
+        mGridView = (StaggeredGridView) findViewById(R.id.gridView1);
+        mAdapter = new SampleAdapter(this,android.R.layout.simple_list_item_1, generateData());
+        // do we have saved data?
+        if (savedInstanceState != null) {
+            mData = savedInstanceState.getStringArrayList(SAVED_DATA_KEY);
+        }
+
+        if (mData == null) {
+            mData = generateData();
+        }
+
+        for (String data : mData) {
+            mAdapter.add(data);
+        }
+
+        mGridView.setAdapter(mAdapter);
+        mGridView.setOnScrollListener(this);
+        mGridView.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(SAVED_DATA_KEY, mData);
+    }
+
+    @Override
+    public void onScrollStateChanged(final AbsListView view, final int scrollState) {
+        Log.d(TAG, "onScrollStateChanged:" + scrollState);
+    }
+
+    @Override
+    public void onScroll(final AbsListView view, final int firstVisibleItem, final int visibleItemCount, final int totalItemCount) {
+        Log.d(TAG, "onScroll firstVisibleItem:" + firstVisibleItem +
+                " visibleItemCount:" + visibleItemCount +
+                " totalItemCount:" + totalItemCount);
+        // our handling
+        if (!mHasRequestedMore) {
+            int lastInScreen = firstVisibleItem + visibleItemCount;
+            if (lastInScreen >= totalItemCount) {
+                Log.d(TAG, "onScroll lastInScreen - so load more");
+                mHasRequestedMore = true;
+                onLoadMoreItems();
+            }
+        }
+    }
+
+    private void onLoadMoreItems() {
+        final ArrayList<String> sampleData = generateData();
+        for (String data : sampleData) {
+            mAdapter.add(data);
+        }
+        // stash all the data in our backing store
+        mData.addAll(sampleData);
+        // notify the adapter that we can update now
+        mAdapter.notifyDataSetChanged();
+        mHasRequestedMore = false;
+    }
+
+    private ArrayList<String> generateData() {
+        ArrayList<String> listData = new ArrayList<String>();
+
+
+        listData.add("http://imageshack.com/a/img743/2009/f1WUYk.jpg");
+        listData.add("http://imageshack.com/a/img537/5299/jXkVNF.jpg");
+        listData.add("http://imageshack.com/a/img537/7525/UNLZy0.jpg");
+        listData.add("http://imageshack.com/a/img745/2618/vK6mEZ.jpg");
+        listData.add("http://imageshack.com/a/img633/1379/O1xbmy.jpg");
+        listData.add("http://imageshack.com/a/img905/4750/IOQojQ.jpg");
+        listData.add("http://imageshack.com/a/img540/4620/aW4mTM.jpg");
+        listData.add("http://imageshack.com/a/img673/9353/cqea2v.jpg");
+        listData.add("http://imageshack.com/a/img540/9319/FO0NKK.jpg");
+        listData.add("http://imageshack.com/a/img910/6761/WmgmPl.jpg");
+        listData.add("http://imageshack.com/a/img540/1564/zkxtdv.jpg");
+        listData.add("http://imageshack.com/a/img538/7937/zU10VS.jpg");
+        listData.add("http://imageshack.com/a/img537/4130/C6FWzr.jpg");
+        listData.add("http://imageshack.com/a/img912/9061/gM60sN.jpg");
+        listData.add("http://imageshack.com/a/img661/2456/UvwXA0.jpg");
+
+
+
+        return listData;
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, "Item Clicked: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+    private static final String TAG = "StaggeredGridActivity";
+    public static final String SAVED_DATA_KEY = "SAVED_DATA";
+
+    private StaggeredGridView mGridView;
+    private boolean mHasRequestedMore;
+    private SampleAdapter mAdapter;
+
+    private Integer[] wallpaperThumbIntegers = {
+            R.drawable.image_1, R.drawable.image_2,
+            R.drawable.image_3, R.drawable.image_4,
+            R.drawable.image_5, R.drawable.image_6,
+            R.drawable.image_7, R.drawable.image_8,
+            R.drawable.image_9,R.drawable.image_10,
+            R.drawable.image_11,R.drawable.image_12,
+            R.drawable.image_13,R.drawable.image_14,
+            R.drawable.image_15
+
+    };
+
+    private ArrayList<String> mData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        setTitle("TechnoTalkative - SGV Demo");
+        mGridView = (StaggeredGridView) findViewById(R.id.gridView1);
+        mAdapter = new SampleAdapter(this,android.R.layout.simple_list_item_1, generateData());
+        // do we have saved data?
+        if (savedInstanceState != null) {
+            mData = savedInstanceState.getStringArrayList(SAVED_DATA_KEY);
+        }
+
+        if (mData == null) {
+            mData = generateData();
+        }
+
+        for (String data : mData) {
+            mAdapter.add(data);
+        }
+
+        mGridView.setAdapter(mAdapter);
+        mGridView.setOnScrollListener(this);
+        mGridView.setOnItemClickListener(this);
+
+      //  StaggeredGridView gridView = (StaggeredGridView) findViewById(R.id.gridView1);
+      //  gridView.setAdapter(new ImageAdapter(this));
+
+
+
+    }
+
+
+
+    @Override
+    public void onScrollStateChanged(final AbsListView view, final int scrollState) {
+        Log.d(TAG, "onScrollStateChanged:" + scrollState);
+    }
+
+    @Override
+    public void onScroll(final AbsListView view, final int firstVisibleItem, final int visibleItemCount, final int totalItemCount) {
+        Log.d(TAG, "onScroll firstVisibleItem:" + firstVisibleItem +
+                " visibleItemCount:" + visibleItemCount +
+                " totalItemCount:" + totalItemCount);
+        // our handling
+        if (!mHasRequestedMore) {
+            int lastInScreen = firstVisibleItem + visibleItemCount;
+            if (lastInScreen >= totalItemCount) {
+                Log.d(TAG, "onScroll lastInScreen - so load more");
+                mHasRequestedMore = true;
+                onLoadMoreItems();
+            }
+        }
+    }
+
+    private void onLoadMoreItems() {
+        final ArrayList<String> sampleData = generateData();
+        for (String data : sampleData) {
+            mAdapter.add(data);
+        }
+        // stash all the data in our backing store
+        mData.addAll(sampleData);
+        // notify the adapter that we can update now
+        mAdapter.notifyDataSetChanged();
+        mHasRequestedMore = false;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Toast.makeText(this, "Item Clicked: " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    private ArrayList<String> generateData() {
+        ArrayList<String> listData = new ArrayList<String>();
+        listData.add("http://i62.tinypic.com/2iitkhx.jpg");
+        listData.add("http://i61.tinypic.com/w0omeb.jpg");
+        listData.add("http://i60.tinypic.com/w9iu1d.jpg");
+        listData.add("http://i60.tinypic.com/iw6kh2.jpg");
+        listData.add("http://i57.tinypic.com/ru08c8.jpg");
+        listData.add("http://i60.tinypic.com/k12r10.jpg");
+        listData.add("http://i58.tinypic.com/2e3daug.jpg");
+        listData.add("http://i59.tinypic.com/2igznfr.jpg");
+
+        return listData;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+/*
+    protected class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return wallpaperThumbIntegers.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+            ImageView imageView;
+
+            if(convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(216, 162));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(wallpaperThumbIntegers[position]);
+            return imageView;
+        }
+    }
+*/
+
+
+
+}
---
+++ app/src/main/java/com/witmergers/batmanwallpapers/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.witmergers.batmanwallpapers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+/**
+ * Created by WP8Dev on 15-10-2014.
+ */
+public class SplashScreen extends Activity {
+
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Handler to run Main activity
+        setContentView(R.layout.activity_splashscreen);
+        Toast.makeText(getApplicationContext(), "Loading...", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
+        Toast.makeText(getApplicationContext(), "Finished Splash Screen...", Toast.LENGTH_SHORT).show();
+    }
+}
